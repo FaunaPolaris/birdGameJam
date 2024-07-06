277b36251bd2d4d04ded86b9d368caf8aa52ac2c
metavenoma <veronauan@gmail.com>
2024-07-06 19:47:57 -0300

    + pica-pau animation change in pica-pau range and procedural generation

--- scenes/Colectables/ovo.gd
+++ scenes/Colectables/ovo.gd
@@ -7,10 +7,16 @@ func _process(delta):
 	if self.has_overlapping_bodies():
 		if 		get_parent().get_parent().get_parent().get_node("Player").hunger < 10:
 			get_parent().get_parent().get_parent().get_node("Player").hunger += 3
-		var defensors : int = abs(randf_range(0, 5))
+		var defensors : int = randi_range(4, 8)
 		for enemy in defensors:
 			var instance = bird.instantiate()
-			instance.position.x = get_parent().get_parent().get_parent().get_node("Player").position.x + 2040 + randf_range(0, 3600)
-			instance.position.y = randf_range(56, 1024)
+			instance.position.x = get_parent().get_parent().get_parent().get_node("Player").position.x + 2040 + randf_range(0, 512)
+			var playerY = get_parent().get_parent().get_parent().get_node("Player").position.y
+			if playerY - 128 < 56:
+				instance.position.y = randf_range(56, 312)
+			elif playerY + 128 > 1024:
+				instance.position.y = randf_range(768, 1024)
+			else:
+				instance.position.y = randf_range(playerY - 128, playerY + 128)
 			get_parent().get_parent().get_parent().add_child(instance)
 		get_parent().queue_free()
